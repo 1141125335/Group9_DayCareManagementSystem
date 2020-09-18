b85658e16718d174ad2738782370f78e499a887d
Yong Xiang <yong.xiang97@gmail.com>
2020-09-18 20:42:22 +0800

    Fix menu "Result" tab naming, by right it should be "Gellery"

--- daycare/foodschedule.php
+++ daycare/foodschedule.php
@@ -118,7 +118,7 @@ HTML;
 				<li><a href="Payment.php">Payments</a></li>
 				<li><a href="foodschedule.php">Food</a></li>
 				<li><a href="timetable.php">Timetable</a></li>
-				<li><a href="gallery.php">Result</a></li>
+				<li><a href="gallery.php">Gallery</a></li>
 				<?php 
 
 				if($_SESSION['permission'] == '1')
