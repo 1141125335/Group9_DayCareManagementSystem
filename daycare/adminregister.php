25b084df4ebe3d4cddf0b4472994cb41916e48ca
Yong Xiang <yong.xiang97@gmail.com>
2020-09-19 23:21:49 +0800

    Fix menu no focus on current page but home page only

--- daycare/adminregister.php
+++ daycare/adminregister.php
@@ -84,16 +84,16 @@ function submitregister()
   <div id="menu-wrapper">
     <div id="menu" class="container">
       <ul>
-        <li class="current_page_item"><a href="index.php">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="Payment.php">Payments</a></li>
         <li><a href="foodschedule.php">Food</a></li>
         <li><a href="timetable.php">Timetable</a></li>
-        <li><a href="gallery.php">Result</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
         <?php 
 
         if($_SESSION['permission'] == '1')
         {
-          echo '<li><a href="adminregister.php">Register</a></li>';
+          echo '<li class="current_page_item"><a href="adminregister.php">Register</a></li>';
         }
         ?>        
         <li><a href="javascript:void(0)" onclick="changePermission();">Permission </a></li>
--- daycare/timetable.php
+++ daycare/timetable.php
@@ -109,10 +109,10 @@ HTML;
 <div id="menu-wrapper">
     <div id="menu" class="container">
       <ul>
-        <li class="current_page_item"><a href="index.php">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="Payment.php">Payments</a></li>
         <li><a href="foodschedule.php">Food</a></li>
-        <li><a href="timetable.php">Timetable</a></li>
+        <li class="current_page_item"><a href="timetable.php">Timetable</a></li>
         <li><a href="gallery.php">Gallery</a></li>
         <?php 
 
